38e60cd56beced714858e20db3086f834c379d5f
Kevin-rm <kevinramarozatovo@gmail.com>
2025-01-04 19:25:05 +0300

    make package-lock.json ignored

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -5,7 +5,7 @@ import ContentLayout from "@/components/content-layout";
 
 export default function LogsPage(): JSX.Element {
   return (
-    <ContentLayout breadcrumbItems={[{label: "autres", link: "#"}, {label: "Logs"}]}>
+    <ContentLayout breadcrumbItems={[{label: "Logs"}]}>
       Hello
     </ContentLayout>
   );
